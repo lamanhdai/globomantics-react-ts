3ec455f3563eaa56cd78e54818db81079767c390
bruce.lam <bruce.lam@websparks.sg>
2022-11-10 00:36:08 +0700

    update existing js to ts

---
+++ app/src/context/AuthProvider.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+interface AuthContextValues {
+  authInfo: AuthInfo
+  isAuthenticated: boolean
+  setAuthInfo: (authInfo: AuthInfo) => void
+  isAdmin: boolean
+}
+
+export const AuthContext = React.createContext<undefined | AuthContextValues>(undefined);
+const Provider = AuthContext.Provider;
+
+interface Props {
+  children: React.ReactNode
+}
+
+interface UserData {
+  role: string
+}
+
+interface AuthInfo {
+  userData: UserData | null
+}
+
+export function AuthProvider({ children }: Props) {
+  const [authInfo, setAuthInfo] = React.useState<AuthInfo>({
+    userData: null,
+  });
+
+  const isAuthenticated = authInfo.userData !== null;
+
+  const isAdmin = authInfo.userData?.role === "ADMIN";
+
+  function handleAuthInfo(authInfo: AuthInfo) {
+    setAuthInfo(authInfo)
+  }
+
+  return (
+    <Provider value={{ authInfo, isAuthenticated, setAuthInfo: handleAuthInfo, isAdmin }}>
+      {children}
+    </Provider>
+  );
+}
+
+
+export function useAuthContext() {
+  const context = React.useContext(AuthContext)
+
+  if(context === undefined) {
+    throw new Error('useAuthContext must use under AuthProvider')
+  }
+  return context;
+}
